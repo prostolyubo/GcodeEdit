c83da86b1cfec51c6da7b0b0bf53b8e6f2bac7fd
Michael Blankenship <Blankenship@OriginCodeAcademy.com>
2018-04-28 13:12:02 -0700

    v1.0.4 Added iron and v/version

--- main.go
+++ main.go
@@ -23,12 +23,18 @@ import (
 	"strings"
 )
 
+func showversion() {
+	fmt.Printf("v1.0.4\n")
+	os.Exit(0)
+}
+
 func syntax(more string) {
 	fmt.Printf("Syntax: GcodeEdit [flags] GCodeFilePath\n\n")
 	fmt.Printf("        -t1=190                 (sets first extruder temperature in celcius)\n")
 	fmt.Printf("        -dryrun                 (no fans, no heatup, no extrusion)\n")
-	fmt.Printf("        -v                      (verbose)\n")
+	fmt.Printf("        -verbose                (verbose)\n")
 	fmt.Printf("        -info                   (displays layer information in file only)\n")
+	fmt.Printf("        --version | -v          (displays version information)\n")
   if (more != "") {
 		fmt.Printf("\nError: %s\n", more)
 	}
@@ -44,6 +50,7 @@ func commandInList(a string, list []string) bool {
 func main() {
 	bReadError                      := false
 	bHeader                         := true
+	bNotReplayedYet                 := true
 	currentlayer                    := -999
 	firstlayer                      := -999
 	slicer                          := "N/A"
@@ -51,18 +58,33 @@ func main() {
 	layers                          := "N/A"
 	inputfilename                   := "N/A"
 	outputfilename                  := "N/A"
+	firstironline                   := ";N/A"
+	saveline                        := ";N/A"
+	optioncount                     := 0
+	ironlayer                       := 999
+	linestoreplay                   := make([]string,0)
 	dataOut, err                    := os.Create("/tmp/GcodeEdit")
 	t1                              := flag.Int("t1",        -1,    "set first extruder to indicated temp [-t1=190]")
+	iron                            := flag.Int("iron",      -99,   "iron the indicated layer [-iron=3]")
 	dryrun                          := flag.Bool("dryrun",   false, "no heat/extrusion/fans")
-	verbose                         := flag.Bool("v",        false, "verbose output")
+	verbose                         := flag.Bool("verbose",  false, "verbose output")
 	info                            := flag.Bool("info",     false, "just read layer data from file")
+	v                               := flag.Bool("v",        false, "show version information")
+	version                         := flag.Bool("version",  false, "show version information")
 	heatrelated	                    := []string{"M101", "M102", "M103", "M104", "M106", "M107",
 		                              "M109", "M116", "M128", "M140", "M141", "M190", "M191"}
-  flag.Parse()
-	if len(flag.Args()) != 1               { syntax("") }		// Should be only one argument as a filename after flags
-	if (! *dryrun && *t1 == -1 && ! *info) { syntax("Minimally, add a command flag to perform an operation on the file") }
-	if (*verbose && ! *dryrun)             { syntax("Use -v with -dryrun") }
-	if (*dryrun && *t1 != -1)              { syntax("Either -dryrun or -t1 flag but not both") }
+	flag.Parse()
+	if (*v && (*dryrun || *iron != -99))                 { syntax("Use -verbose with -dryrun or -iron; -v shows version info") }
+  if (*v || *version)                                  { showversion() }
+	if len(flag.Args()) != 1                             { syntax("") }		// Should be only one argument as a filename after flags
+	if (!*dryrun && *t1 == -1 && !*info && *iron == -99) { syntax("Minimally, add a command flag to perform an operation on the file") }
+	if (*verbose && ! (*dryrun || *iron != -99))         { syntax("Use -verbose with -dryrun or -iron") }
+	if (*iron <= 1 && *iron > -99)                       { syntax("Minimum -iron value should be 2") }
+	if (*dryrun) {optioncount++}
+	if (*t1 != -1) {optioncount++}
+	if (*iron != -99) {optioncount++}
+	if optioncount > 1                                   { syntax("Only one of: -dryrun, -t1 or -iron flags at a time") }
+  if (*iron != -99)                                    { ironlayer = *iron - 1 }
 	/*
 	dryrun-related
 	M104 Set extruder temp         	  M109 Set extruder temp and wait
@@ -103,7 +125,47 @@ func main() {
 			if firstlayer == -999 {
 				firstlayer = currentlayer
 			}
-		}
+		} else {
+		// ----------------------------------------------------------------------
+		// Processing for iron command
+		// ----------------------------------------------------------------------
+		if *iron != -1 && ironlayer == currentlayer {
+				if firstironline == ";N/A" {
+					// Haven't seen the to-be-ironed layer before but we're there now
+					firstironline = line
+					s := fmt.Sprintf(";LAYER:%d replay per iron-processing command: GcodeEdit)", currentlayer)
+					linestoreplay = append(linestoreplay, s)
+					if (line[:1] != "M") {
+						linestoreplay = append(linestoreplay, line)
+					}
+				} else {
+					if (strings.Contains(line, " E") && (line[:2] == "G0" || line[:2] == "G1")) {
+						// Some sort of extrusion is taking place; must remove that part
+						if (*verbose) {fmt.Printf("  Before:    %s\n", line)}
+						re := regexp.MustCompile(" E([0-9\\.]+)")
+						saveline = re.ReplaceAllString(line, "");
+						if (*verbose) {fmt.Printf("  After:     %s\n", line)}
+						orphanedFCommand, err := regexp.MatchString("G[0-1] F([0-9]+)[ ]?$", saveline)
+						if (err == nil && orphanedFCommand) {
+							// In some cases, this then leaves nothing but G1 F2400 left after
+							// the removal of the extrusion command; let's lose that, too.
+							saveline = ";" + saveline
+							if (*verbose) {fmt.Printf("  Commented: %s\n", saveline)}
+						} // if (err == nil
+						linestoreplay = append(linestoreplay, saveline)
+					} else if (line[:1] == ";") {
+						linestoreplay = append(linestoreplay, line)
+					}   // else if (strings.Contains(line, " E")
+				}     // else if firstironline == ";N/A"
+			}       // if *iron != -1
+ 		}         // else if strings.Contains(line, ";LAYER:")
+		// ----------------------------------------------------------------------
+		// End of iron processing, see below as well
+		// ----------------------------------------------------------------------
+
+		// ----------------------------------------------------------------------
+		// Header processing (info and t1)
+		// ----------------------------------------------------------------------
 		if bHeader {
 			// M104 S190
 			if strings.Contains(line, "M104") || strings.Contains(line, "M109") {
@@ -120,10 +182,16 @@ func main() {
 				layers = line[13:]
 			}
 		} // end of if bHeader
+		// ----------------------------------------------------------------------
+		// End of header processing
+		// ----------------------------------------------------------------------
 		if (! *info) {
 			if line == "\n" {
 				dataOut.WriteString("\n")
 			} else {
+				// ------------------------------------------------------------------
+				// dryrun processing
+				// ------------------------------------------------------------------
 				if (*dryrun && len(line) > 1) {
 					if (commandInList(line[:4], heatrelated)) {
 						// dryrun action: comment-out M104 and similar heat-related commands
@@ -145,9 +213,27 @@ func main() {
 						}   // if (strings.Contains(line, " E")
 					}     // if (commandInList(
 				}       // if (*dryrun && len(line) > 1)
+				// ------------------------------------------------------------------
+				// end of dryrun processing
+				// ------------------------------------------------------------------
+
+				// ------------------------------------------------------------------
+				// Pre-processing for replay of iron
+				// ------------------------------------------------------------------
+				if *iron != -1 && (ironlayer + 1) == currentlayer && bNotReplayedYet {
+					for _, val := range linestoreplay {
+						dataOut.WriteString(val + "\n")
+					}     // for _, val
+					dataOut.WriteString(";End of iron-processing replay: GcodeEdit\n")
+					bNotReplayedYet = false
+				}       // if *iron != -1
+				// ------------------------------------------------------------------
+				// End of iron processing
+				// ------------------------------------------------------------------
 
 				// line has been processed, now write it to the file
 				dataOut.WriteString(line + "\n")
+
 			}         // else for if line == "\n"
 		}           // if ! *info
 	}             // for loop
@@ -163,6 +249,14 @@ func main() {
 			fmt.Printf("  Output filename:  %s\n", outputfilename)
 			if (*t1 != -1)                    { fmt.Printf("  Temp now:         %dC\n", *t1) }
 			if (*dryrun)                      { fmt.Printf("  No heat/extrusion/fans\n") }
+			if (*iron != -99) {
+				fmt.Printf("  Replayed lines:   %d\n", len(linestoreplay))
+				if (*verbose) {
+					for k, val := range linestoreplay {
+						fmt.Printf("  Replay[%d]: %s\n", k, val)
+					}
+				}
+			}
 		}
 		fmt.Printf("\nFinished.\n\n")
 	}
